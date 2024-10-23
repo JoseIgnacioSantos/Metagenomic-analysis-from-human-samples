7f9238f5169ace855536f2d760acd7590a17b18e
JoseIgnacioSantos <isantosfr@gmail.com>
2024-10-23 13:09:46 +0200

    Add files via upload

--- download_clean.sh
+++ download_clean.sh
@@ -1,4 +1,4 @@
-i!/bin/bash
+#!/bin/bash
 # Este pipeline tiene como objetivo leer un fichero input de lista de acceso de secuencias
 # para descargar todos los ficheros SRR de su interior y filtrarlos con fastqc. Funciona con secuencias paired-end
 # El script se debe situar donde el fichero de secuencias a descargar
@@ -19,17 +19,10 @@ echo "la lista de secuencias ha sido descargada correctamente"
 #En caso de que el directorio exista, saltará un aviso de que ya existe y no creará ninguno nuevo
 
 echo "Creando directorio para almacenar las secuencias descargadas..."
-for n in {1..10}
+n=0
+while ! mkdir fastq_files$n
 do
-	if [ ! -d ./fastq_files_$n];then
-		mkdir fastq_files_$n
-		echo "El directorio fastq_files_$n se ha creado con éxito"
-		break
-	else 
-		$n+=1
-		echo "Los directorios anteriores YA EXISTEN, creando nuevos directorios..."
-		mkdir fastq_files_$n
-	fi
+	n=$((n+1))
 done
 
 echo "moviendo las secuencias al nuevo directorio"
